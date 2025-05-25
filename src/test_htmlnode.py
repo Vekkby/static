0d57f121988e4499028d75ad7fa0ef16c5dd4f20
Nikita Mullin <39054595+Vekkby@users.noreply.github.com>
2025-05-25 16:01:46 +0200

    Add htmlnode

---
+++ src/test_htmlnode.py
@@ -0,0 +1,29 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+
+class TestTextNode(unittest.TestCase):
+    def test_to_html(self):
+        node= HTMLNode()
+        
+        self.assertRaises(NotImplementedError, node.to_html)
+
+    def props_to_html(self):
+        props = {'key': 'value', 'other_key': 'other_value'} 
+        node = HTMLNode(props=props) 
+
+        self.assertEqual(node, 'key=value other_key=other_value')
+
+    def test_repr(self):
+        tag = 'tag'
+        value = 'value'
+        children = [HTMLNode(), HTMLNode()]
+        props = {'key': 'value', 'other_key': 'other_value'} 
+        node = HTMLNode(tag, value, children, props)
+        expected_result = f'HTMLNode({tag} {value} {children} {props})'
+
+        self.assertEqual(str(node), expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
